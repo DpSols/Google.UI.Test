7cdbfdb4195b32d98c6d7771173b56e1b00b825b
Nodir <kebridon.nemat.life@gmail.com>
2022-11-07 03:35:16 +0500

    Add project files.

---
+++ Navigations.cs
@@ -0,0 +1,11 @@
+﻿namespace Google.UI.Test
+{
+    public static class Navigations
+    {
+        // Locators for selenium webdriver
+        // Google search page locators 
+        public static readonly string _url = "https://www.google.com/";
+        public static readonly string _classNameSearchQuery = "gLFyf";
+        public static readonly string _classNameSearchButton = "gNO89b";
+    }
+}
---
+++ SearchTitleTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Google.UI.Test
+{
+    [TestFixture]
+    internal class SearchTitleTest
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void GoogleSearch()
+        {
+            using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+            
+            _driver.Navigate().GoToUrl(Navigations._url);
+            _driver.FindElement(By.ClassName(Navigations._classNameSearchQuery)).SendKeys("Hello World");
+            _driver.FindElement(By.ClassName(Navigations._classNameSearchButton)).Click();
+            var PageTitle = _driver.Title;
+            logger.LogInformation(PageTitle);
+        }
+    }
+}
